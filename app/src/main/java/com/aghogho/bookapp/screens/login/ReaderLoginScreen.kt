209e39dd3a4ef537238ac64aa240e4d3dc0d8971
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-05-25 13:32:54 +0300

    create functionality to animate splash screen and navigate to loginScreen after splashscreen animation completes

--- app/src/main/java/com/aghogho/bookapp/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/aghogho/bookapp/screens/login/ReaderLoginScreen.kt
@@ -1,9 +1,10 @@
 package com.aghogho.bookapp.screens.login
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun LoginScreen(navController: NavController) {
-
+    Text(text = "Login Screen")
 }
