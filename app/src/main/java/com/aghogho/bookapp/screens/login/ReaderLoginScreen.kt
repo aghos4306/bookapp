0b7c586dd4fd025effcc2707af3ac61c2d810b9d
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-05-26 12:46:04 +0300

    created password field and display form

--- app/src/main/java/com/aghogho/bookapp/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/aghogho/bookapp/screens/login/ReaderLoginScreen.kt
@@ -1,15 +1,15 @@
 package com.aghogho.bookapp.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -19,17 +19,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.aghogho.bookapp.components.EmailInput
+import com.aghogho.bookapp.components.PasswordInput
 import com.aghogho.bookapp.components.ReaderLogo
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginScreen(navController: NavController) {
     androidx.compose.material.Surface(
@@ -41,21 +46,27 @@ fun LoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top
         ) {
             ReaderLogo()
+            UserForm(loading = false, isCreateAccount = false) {email, password ->
+                Log.d("Form", "ReaderLoginScreen: $email $password")
+            }
         }
     }
 }
 
-@Preview
 @ExperimentalComposeUiApi
 @Composable
-fun UserForm() {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = {email, pwd ->}
+) {
     val email = rememberSaveable { mutableStateOf("") }
     val password = rememberSaveable { mutableStateOf("") }
-    val passwordVisibility = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
     val passwordFocusRequest = FocusRequester.Default
     val keyboardController = LocalSoftwareKeyboardController.current
     val valid = remember(email.value, password.value) {
-        email.value.trim().isNotEmpty() && password.value.isNotEmpty()
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
     }
 
     val modifier = Modifier
@@ -63,16 +74,35 @@ fun UserForm() {
         .background(MaterialTheme.colors.background)
         .verticalScroll(rememberScrollState())
 
-    Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier,
+        horizontalAlignment = Alignment.CenterHorizontally) {
         EmailInput(
             emailState = email,
-            enabled = true,
+            enabled = !loading,
             onAction = KeyboardActions {
                 passwordFocusRequest.requestFocus()
             }
         )
+        PasswordInput(
+            modifier = Modifier
+                .focusRequester(passwordFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+                if(!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            }
+        )
     }
-
 }
+
+
+
+
+
+
 
 
