4704ca6f7d53fa7b3d75913704071fd0fb44df90
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-05-26 21:00:33 +0300

    build static FAB

--- app/src/main/java/com/aghogho/bookapp/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/aghogho/bookapp/screens/home/ReaderHomeScreen.kt
@@ -1,10 +1,40 @@
 package com.aghogho.bookapp.screens.home
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Text(text = "Welcome to Home Screen")
+    Scaffold(
+        topBar = {},
+        floatingActionButton = {
+            FABContent{}
+        }
+    ) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            //Home Content
+        }
+    }
+}
+
+@Composable
+fun FABContent(onTap: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap() },
+        shape = RoundedCornerShape(50.dp),
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add a Book",
+            tint = MaterialTheme.colors.onSecondary
+        )
+    }
 }
