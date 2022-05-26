125f34a1da8222a52036106bc2f8ad2e949906e1
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-05-26 14:42:10 +0300

    create loadingstate resource for handling success, error, loading response. Create also viewmodel logic to handle singInWithEmailAndPassword.

---
+++ app/src/main/java/com/aghogho/bookapp/screens/login/LoginScreenViewModel.kt
@@ -0,0 +1,42 @@
+package com.aghogho.bookapp.screens.login
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class LoginScreenViewModel: ViewModel() {
+    //val loadingState = MutableStateFlow(LoadingState.IDLE)
+    private val auth: FirebaseAuth = Firebase.auth
+
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean> = _loading
+
+
+    fun signInWithEmailAndPassword(email: String, password: String) = viewModelScope.launch {
+        try {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        //Todo: Take User to Home Screen
+                    } else {
+                        Log.d("Firebase", "signInWithEmailAndPassword: ${task.result.toString()}")
+                    }
+                }
+        } catch (ex: Exception) {
+            Log.d("Firebase SignIn", "singInWithEmailAndPassword: ${ex.message}")
+        }
+    }
+
+    fun createUserWithEmailAndPassword() {
+
+    }
+
+
+}
