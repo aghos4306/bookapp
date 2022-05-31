2fb93c3aea54cb0e10cb3e62af8c04db5c828535
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-05-31 20:08:42 +0300

    created the topbar for homescreen. Created also navigation route to stats screen. Created also the logic to extract name from login users and use the naem on profile

--- app/src/main/java/com/aghogho/bookapp/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/aghogho/bookapp/screens/home/ReaderHomeScreen.kt
@@ -1,32 +1,37 @@
 package com.aghogho.bookapp.screens.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Book
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.aghogho.bookapp.R
+import com.aghogho.bookapp.model.MBook
 import com.aghogho.bookapp.navigation.ReaderScreens
 import com.google.firebase.auth.FirebaseAuth
 
+@Preview
 @Composable
-fun HomeScreen(navController: NavController) {
+fun HomeScreen(navController: NavController = NavController(LocalContext.current)) {
     Scaffold(
         topBar = {
                  ReaderAppBar(
@@ -40,6 +45,7 @@ fun HomeScreen(navController: NavController) {
     ) {
         Surface(modifier = Modifier.fillMaxSize()) {
             //Home Content
+            HomeContent(navController)
         }
     }
 }
@@ -81,7 +87,8 @@ fun ReaderAppBar(
                   }) {
                       Icon(
                           imageVector = Icons.Default.Logout,
-                          contentDescription = "Logout Icon"
+                          contentDescription = "Logout Icon",
+                          //tint = Color.Green.copy(alpha = 0.4f)
                       )
                   }
         },
@@ -104,3 +111,87 @@ fun FABContent(onTap: () -> Unit) {
         )
     }
 }
+
+@Composable
+fun ReadingRightNowArea(
+    books: List<MBook>,
+    navController: NavController
+) {
+
+}
+
+@Composable
+fun TitleSection(
+    modifier: Modifier = Modifier,
+    label: String
+) {
+    Surface(
+        modifier = Modifier
+            .padding(start = 5.dp, top = 1.dp)
+    ) {
+        Column {
+            Text(
+                text = label,
+                fontSize = 19.sp,
+                fontStyle = FontStyle.Normal,
+                textAlign = TextAlign.Left
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeContent(
+    navController: NavController
+) {
+    val email = FirebaseAuth.getInstance().currentUser?.email
+    val currentUserName = if (!email.isNullOrEmpty()) {
+        FirebaseAuth.getInstance().currentUser?.email?.split("@")?.get(0)
+    } else {
+        "Name n/a"
+    }
+    Column(
+        Modifier.padding(2.dp),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Row(
+            modifier = Modifier
+                .align(alignment = Alignment.Start)
+        ) {
+            TitleSection(label = " Your reading \n " +"activity right now...")
+            Spacer(modifier = Modifier.fillMaxWidth(0.7f))
+            Column {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate(ReaderScreens.ReaderStatsScreen.name)
+                        }
+                        .size(45.dp),
+                    tint = MaterialTheme.colors.secondaryVariant
+                )
+                Text(
+                    text = currentUserName.toString(),
+                    modifier = Modifier.padding(2.dp),
+                    style = MaterialTheme.typography.overline,
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Clip
+                )
+                Divider()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
